1a7d32d9822cbbf3a36dfc66b8ed342449177316
micahpotts <mrs.micahpotts@gmail.com>
2016-09-10 19:20:52 -0400

    Hid spoiler, added button

--- spolierRevealer/js/app.js
+++ spolierRevealer/js/app.js
@@ -2,7 +2,9 @@
 //Reveal upon click
 
 //hide spoiler
+$(".spoiler span").hide();
 //add a button
+$(".spoiler").append("<button>Reveal Spoiler!</button>");
 //on button press
 	//show spoiler
 	//get rid of button
