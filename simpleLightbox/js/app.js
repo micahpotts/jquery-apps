91eba4e39ea5ccbf1d5022e396151d4911d5735a
micahpotts <mrs.micahpotts@gmail.com>
2016-09-10 21:28:33 -0400

    Added overlay when image clicked

--- simpleLightbox/js/app.js
+++ simpleLightbox/js/app.js
@@ -1,18 +1,23 @@
 //Problem - Goes to dead end when images clicked
 //Solution - Create an overlay with the large image - Lightbox
 
-//1. Capture click event on a link to an image
+var $overlay = $('<div id="overlay"></div>');
+// Add overlay
+$("body").append($overlay);
+	// An image
+	// A caption
+
+
+// Capture click event on a link to an image
 $("#imageGallery a").click(function(event) {
 	event.preventDefault();
 	var href = $(this).attr("href");
-	console.log(href);
+	$overlay.show();
 	// Show overlay
 	// Update overlay with image in link
 	// Get child's alt attribute and set caption
 });
 	
-//2 Add overlay
-	// An image
-	// A caption
-//3 When overlay clicked
+
+// When overlay clicked
 	//  hide overlay
