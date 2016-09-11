1c46c4196468f48c6bb7ed9ddf263bb10091897e
micahpotts <mrs.micahpotts@gmail.com>
2016-09-10 21:07:51 -0400

    On click, preventing following link

--- simpleLightbox/js/app.js
+++ simpleLightbox/js/app.js
@@ -2,9 +2,15 @@
 //Solution - Create an overlay with the large image - Lightbox
 
 //1. Capture click event on a link to an image
+$("#imageGallery a").click(function(event) {
+	event.preventDefault();
+	var href = $(this).attr("href");
+	console.log(href);
 	// Show overlay
 	// Update overlay with image in link
 	// Get child's alt attribute and set caption
+});
+	
 //2 Add overlay
 	// An image
 	// A caption
