16f85d8b9ea6fc624aee9ffc83a63c4d949cdde9
micahpotts <mrs.micahpotts@gmail.com>
2016-09-11 11:39:54 -0400

    Made plan
    How to accomplish task

---
+++ mobileDropdownMenu/js/app.js
@@ -0,0 +1,15 @@
+//Problem - looks bad in small browsers and devices
+//Solution - hide text links and swap them for smaller nav
+
+//create a selecct and append to menu
+//cycle over menu links
+	//create an option
+	//option's value is href of the link
+	//option text is text of the link
+	//append option to select
+//create button
+//bind click to button
+	//go to select's location
+//modify css to hide big nav on small widths and show button and select
+	//modify css to hide select and button on larger widths
+//deal with selected options depending on current page
