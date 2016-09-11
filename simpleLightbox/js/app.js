00bdf28680ac4f9f2a1fa3d11d2f61a4a0463991
micahpotts <mrs.micahpotts@gmail.com>
2016-09-11 10:24:23 -0400

    Overlay with photo appears and disappears on click

--- simpleLightbox/js/app.js
+++ simpleLightbox/js/app.js
@@ -2,22 +2,36 @@
 //Solution - Create an overlay with the large image - Lightbox
 
 var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+
+// An image to overlay
+$overlay.append($image);
+
 // Add overlay
 $("body").append($overlay);
-	// An image
 	// A caption
 
 
 // Capture click event on a link to an image
 $("#imageGallery a").click(function(event) {
 	event.preventDefault();
-	var href = $(this).attr("href");
-	$overlay.show();
-	// Show overlay
+	var imageLocation = $(this).attr("href");
 	// Update overlay with image in link
+	$image.attr("src", imageLocation);
+
+	// Show overlay
+	$overlay.show();
+	
+	
 	// Get child's alt attribute and set caption
 });
 	
 
 // When overlay clicked
+$overlay.click(function() {
 	//  hide overlay
+	$(this).hide();
+});
+	
+
+	
