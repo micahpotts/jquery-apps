3ed9972c409a477b76fd046c99b45bab1cd53b66
micahpotts <mrs.micahpotts@gmail.com>
2016-09-10 20:56:23 -0400

    Planned what needed to be done

---
+++ simpleLightbox/js/app.js
@@ -0,0 +1,12 @@
+//Problem - Goes to dead end when images clicked
+//Solution - Create an overlay with the large image - Lightbox
+
+//1. Capture click event on a link to an image
+	// Show overlay
+	// Update overlay with image in link
+	// Get child's alt attribute and set caption
+//2 Add overlay
+	// An image
+	// A caption
+//3 When overlay clicked
+	//  hide overlay
