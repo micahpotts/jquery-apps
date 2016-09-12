3fc968e3439182230abad2b0b52d2834ae433b6e
micahpotts <mrs.micahpotts@gmail.com>
2016-09-12 19:23:07 -0400

    Initial download (minus any JS) and plann

---
+++ simpleDrawingApplication/js/app.js
@@ -0,0 +1,20 @@
+//Problem: No user interaction. No change to application
+//Solution: When user interacts, cause changes appropriately
+
+
+//when clicking on the control list items
+	//deselect sibling element
+	//select clicked element
+
+//when new color clicked
+	//show or hide colorSelect
+
+//when color sliders change
+	//update new color span
+
+//when add color is pressed
+	//append the color to the controls
+	//select the new color
+
+//on mouse events on the canvas
+	//draw lines
