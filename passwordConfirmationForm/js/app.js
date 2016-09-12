0564281c9eacc1bddcebfbc92de82d66e5b90c46
micahpotts <mrs.micahpotts@gmail.com>
2016-09-12 17:39:06 -0400

    Hid both spans, created function to show span if password is under 8 char

--- passwordConfirmationForm/js/app.js
+++ passwordConfirmationForm/js/app.js
@@ -2,11 +2,22 @@
 //Solution: Hide hints and show them at the right time
 
 //hide hints
+$("form span").hide();
+
+function passwordEvent() {
+	//find out if password is valid
+	if ($(this).val().length > 8) {
+		//hide hint if valid
+		$(this).next().hide();
+	} else {
+		//else show hint
+		$(this).next().show();
+	}
+}
 
 //when event happens on password input
-	//find out if password is valid
-		//hide hint if valid
-		//else show hint
+$("#password").focus(passwordEvent).keyup(passwordEvent);
+	
 
 //when event happens on confirmation input
 	//find out if password and confirmation match
