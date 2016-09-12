0e540a4aca4ef883e5d23f0b6b1e26078a67f276
micahpotts <mrs.micahpotts@gmail.com>
2016-09-12 17:23:09 -0400

    Set plan to fix confirmation form

---
+++ passwordConfirmationForm/js/app.js
@@ -0,0 +1,14 @@
+//Problem: Hints are shown all the time
+//Solution: Hide hints and show them at the right time
+
+//hide hints
+
+//when event happens on password input
+	//find out if password is valid
+		//hide hint if valid
+		//else show hint
+
+//when event happens on confirmation input
+	//find out if password and confirmation match
+		//hide hint if matched
+		//else show hint 
