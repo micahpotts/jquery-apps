98f97fd736ddbaaa62e85967fc61636de43a9aa9
micahpotts <mrs.micahpotts@gmail.com>
2016-09-12 22:52:06 -0400

    Made canvas work

--- simpleDrawingApplication/js/app.js
+++ simpleDrawingApplication/js/app.js
@@ -1,6 +1,10 @@
 //Problem: No user interaction. No change to application
 //Solution: When user interacts, cause changes appropriately
 var color = $(".selected").css("background-color");
+var $canvas = $("canvas");
+var context = $canvas[0].getContext("2d");
+var lastEvent;
+var mousedown = false;
 
 //when clicking on the control list items
 $(".controls").on("click", "li", function() {
@@ -43,8 +47,23 @@ $("#addNewColor").click(function() {
 	
 
 //on mouse events on the canvas
+$canvas.mousedown(function(e) {
+	lastEvent = e;
+	mouseDown = true;
+}).mousemove(function(e) {
 	//draw lines
-
+	if(mouseDown) {
+		context.beginPath();
+		context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+		context.lineTo(e.offsetX, e.offsetY);
+		context.strokeStyle = color;
+		context.stroke();
+		lastEvent = e;
+	}
+}).mouseup(function() {
+	mouseDown = false;
+});
+	
 
 
 
